f2dd65648309fbecd28813ecdc4511e9714adfee
nitish-arali <nitisharali1995@gmail.com>
2022-09-14 14:02:29 +0530

    Practise

---
+++ src/TypeCasting/StringToLong.java
@@ -0,0 +1,17 @@
+package TypeCasting;
+
+import java.util.*;
+import java.io.*;
+
+public class StringToLong {
+    public static long conversion(String[] args) {
+        String S=new String("43");
+        long l=Long.parseLong(S);
+        return l;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+}
