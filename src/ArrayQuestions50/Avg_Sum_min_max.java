d8e74abbd8687e940406e0848c993e41301fef5a
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 12:08:16 +0530

    Practise

---
+++ src/ArrayQuestions50/Avg_Sum_min_max.java
@@ -0,0 +1,28 @@
+package ArrayQuestions50;
+import java.util.*;
+import java.io.*;
+
+public class Avg_Sum_min_max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the size of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Input the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        double avg=0;
+        double sum=0;
+        int min,max;
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        avg=sum/2;
+        System.out.println("Sum of elements: "+sum);
+        System.out.println("Average of elements: "+avg);
+        System.out.println("Minimum element in the array is: "+arr[0]);
+        System.out.println("Maximum element in the array is: "+arr[n-1]);
+    }
+}
