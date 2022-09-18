e4aa5640d1c64af8c10384a10cce11ebd1fd109f
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 12:39:09 +0530

    Practise

---
+++ src/ArrayQuestions50/SumOfEvenNumbers.java
@@ -0,0 +1,23 @@
+package ArrayQuestions50;
+import java.util.*;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the size of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Input the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Sum of Even Numbers: ");
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] % 2 == 0){
+                sum=arr[i]+sum;
+            }
+        }
+        System.out.print(sum+" ");
+    }
+}
