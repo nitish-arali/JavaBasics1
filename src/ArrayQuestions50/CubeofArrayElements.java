cc3c8ef0162f626cdf0e81b001342d0bdc7a67fd
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 14:01:57 +0530

    Practise

---
+++ src/ArrayQuestions50/CubeofArrayElements.java
@@ -0,0 +1,24 @@
+package ArrayQuestions50;
+
+import java.util.Scanner;
+
+public class CubeofArrayElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the size of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] arr1=new int[arr.length];
+        System.out.println("Input the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+            for(int j=0;j<arr1.length;j++){
+                arr1[j]=arr[j]*arr[j]*arr[j];
+            }
+
+        for(int i=0;i<arr1.length;i++){
+        System.out.print(arr1[i]+" ");
+        }
+    }
+}
