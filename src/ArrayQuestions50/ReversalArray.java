956148b1b407a84e4a1401146d555f5ff144b7a3
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 11:56:04 +0530

    Practise

---
+++ src/ArrayQuestions50/ReversalArray.java
@@ -0,0 +1,19 @@
+package ArrayQuestions50;
+import java.util.*;
+import java.io.*;
+
+public class ReversalArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the size of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Input the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
