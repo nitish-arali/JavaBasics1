7e3b7127f38df3e0b3c6258a7a2dd90a9a3d9719
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 14:35:57 +0530

    Practise

--- src/ArrayQuestions50/FibonacciNumbers.java
+++ src/ArrayQuestions50/FibonacciNumbers.java
@@ -8,11 +8,11 @@ public class FibonacciNumbers {
         System.out.println("Fibonacci Series upto: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int fact=0;
+        int fib=0;
         arr[0]=1;
         for (int i = 1; i <= arr.length-1; i++){
-            arr[i]=fact+arr[i-1];
-            fact=arr[i-1];
+            arr[i]=fib+arr[i-1];
+            fib=arr[i-1];
         }
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
