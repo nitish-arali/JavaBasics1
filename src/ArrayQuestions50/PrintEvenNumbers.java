e820c781ea8bde187e0a1cefcb1ae3a2b6fb24b8
nitish-arali <nitisharali1995@gmail.com>
2022-09-18 12:15:03 +0530

    Practise

---
+++ src/ArrayQuestions50/PrintEvenNumbers.java
@@ -0,0 +1,21 @@
+package ArrayQuestions50;
+import java.util.*;
+import java.io.*;
+
+public class PrintEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the size of an array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Input the elements of an array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i] %2==0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
