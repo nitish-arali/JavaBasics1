ec8512e7df5a3c1595e6d096c9293a74f6005388
nitish-arali <nitisharali1995@gmail.com>
2022-09-19 13:23:12 +0530

    Practise

---
+++ src/ArrayQuestions50/TwoDArrayPrinting.java
@@ -0,0 +1,28 @@
+package ArrayQuestions50;
+import java.util.*;
+import java.io.*;
+
+
+public class TwoDArrayPrinting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of Rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Number of Columns: ");
+        int col=sc.nextInt();
+        int[][] arr = new int[rows][col];
+        System.out.println("Enter array elements");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("The Matrix looks like");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
