0755ea6286d557f9fc0f6f4c44f837b3c7285125
nitish-arali <nitisharali1995@gmail.com>
2022-10-12 23:33:53 +0530

    Practise

---
+++ src/Strings/CheckTwoStringsAreAnagrams.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class CheckTwoStringsAreAnagrams {
+    static boolean CheckAnagram(String str1, String str2){
+        int count =0;
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+        else {
+            for(int i=0;i<str1.length();i++){
+                for(int j=0;j<str2.length();j++){
+                    if(str1.charAt(i)==str2.charAt(j)){
+                        count++;
+                    }
+                }
+            }
+            if(count==str1.length() && count==str2.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str1="abcd";
+        String str2="bacd";
+        if(CheckAnagram(str1,str2)==true){
+            System.out.println("The given strings are Anagrams");
+        }
+        else System.out.println("The given strings are not Anagrams");
+    }
+}
