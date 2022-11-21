6ba5254a72790c61a0bb2d329d42d7a5f240e08e
nitish-arali <nitisharali1995@gmail.com>
2022-11-21 21:19:01 +0530

    Practise

---
+++ src/Stacks/ValidParanthesis.java
@@ -0,0 +1,30 @@
+package Stacks;
+import java.util.*;
+
+public class ValidParanthesis {
+    public static boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(')
+                st.push(')');
+            else if(c=='{')
+                st.push('}');
+            else if(c=='[')
+                st.push(']');
+            else{
+                if(st.isEmpty() || c!=st.peek())
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("{}()"));
+    }
+}
